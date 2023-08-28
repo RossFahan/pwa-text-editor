7e8b30ff23ffce61b7bc9650e1db700beeb01017
Ross F <rfahan@gmail.com>
2023-08-27 20:25:13 -0600

    added code to retrieve and add content to/from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,39 @@
+// Install the following package:
 import { openDB } from 'idb';
 
-const initdb = async () =>
+// Initialize the database
+const initDb = async () => {
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
     },
   });
+};
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add content to the database
+export const putDb = async (content) => {
+  console.log('Putting content into the database:', content);
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add({ content });
+  await tx.done;
+  console.log('Content added to the database:', content);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Retrieve all content from the database
+export const getDb = async () => {
+  console.log('Getting all content from the database');
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = await store.getAll();
+  await tx.done;
+  console.log('All content retrieved from the database:', allContent);
+  return allContent;
+};
 
-initdb();
+initDb();
