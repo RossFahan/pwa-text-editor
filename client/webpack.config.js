4aeece9911b8756aa1325546f1736232a643634c
Ross F <rfahan@gmail.com>
2023-08-27 18:33:56 -0600

    configured webpack.cong.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,53 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Generate HTML file
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+
+      // Generate the Web App Manifest
+      new WebpackPwaManifest({
+        name: 'Your Text Editor App',
+        short_name: 'Text Editor',
+        description: 'A Progressive Web App Text Editor',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
+
+      // Inject your service worker
+      new InjectManifest({
+        swSrc: './src-sw.js', // Your service worker source file
+        swDest: 'service-worker.js', // The output service worker file
+      }),
     ],
 
     module: {
       rules: [
-        
+        // CSS loader
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+
+        // Babel loader for modern JavaScript
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
